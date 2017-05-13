0799a74553127b851919b09d32f6905f2779085c
Ryan McCormick <rmccorm4@binghamton.edu>
2017-05-13 18:28:47 -0500

    start parser

--- AST.py
+++ AST.py
@@ -45,4 +45,74 @@ class FunctionNode(object):
 
 
 ################ PARSER ##################
+class Parser(object):
+	def __init__(self, tokens, binop_precedence):
+		self.tokens = tokens
+		self.binop_precedence = binop_precedence
+		self.Next()
 
+	# Provide a simple token buffer.
+	# Parser.current is the current token being looked at by parser
+	# Parser.Next() reads another token from lexer and updates parser.current
+	def Next(self):
+		self.current = self.tokens.next()
+
+	# Handle expressions with numbers
+	def ParseNumberExpr(self):
+		result = NumberExpressionNode(self.current.value)
+		# consume the number
+		self.Next()
+		return result
+
+	# Handle expressions with parentheses
+	def ParseParenExpr(self):
+		# eat '('
+		self.Next()
+		contents = self.ParseExpression()
+		if self.current != CharacterToken(')'):
+			raise RuntimeError('Exprected ")".')
+		# eat ')'
+		self.Next()
+		return contents
+
+	# Handling variable references and function calls
+	def ParseIdentifierExpr(self):
+		identifierName = self.current.name
+		# eat identifier
+		self.Next()
+
+		# If identifier not followed parentheses, then its a variable
+		if self.current != CharacterToken('('):
+			return VariableExpressionNode(identifierName)
+
+		# If identifier followed by parentheses, its a function call
+		# eat '('
+		self.Next()
+		args = []
+		if self.current != CharacterToken(')'):
+			while True:
+				args.append(self.ParseExpression())
+				# If there are no more arguments
+				if self.current == CharacterToken(')'):
+					break
+				# If there was another token without a comma separator
+				elif self.current != CharacterToken(','):
+					raise RuntimeError('Expected ")" or "," in argument list.')
+				self.Next()
+		
+		# eat ')'
+		self.Next()
+		return CallExpressionNode(identifierName, args)
+	
+	# Helper function to wrap everything into one entry point
+	def ParsePrimary(self):
+		if isinstance(self.current, IdentifierToken):
+			return self.ParseIdentifierExpr()
+		elif isinstance(self.current, NumberToken):
+			return self.ParseNumberExpr()
+		elif self.current == CharacterToken(')'):
+			return self.ParseParenExpr()
+		else:
+			raise RuntimeError('Unknown token when expecting an expression.')
+	
+# Next part here starts at main/driver code		
